689d5926accd769f25f9401ef6f1dd9612f71a58
Nasandratra <andriankotomiarintsoa24@gmail.com>
2025-10-04 09:56:33 +0100

    check render

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: "/Portfolio-ANDRIANKOTOMIARINTSOA/", // <-- le nom de ton repo GitHub
+=======
+  base: "/", // très important : racine du site
+>>>>>>> 7f85f7f0 (check render)
 });
